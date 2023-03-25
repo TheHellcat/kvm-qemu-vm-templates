217f5df2b0eb9840b79398193f7f6b651095ac78
Hellcat <hellcat@hellcat.net>
2023-03-25 19:39:00 +0100

    commited everything

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # kvm-qemu-vm-templates
+
 A small collection of tempate XMLs and scripts to easiely create a KVM/QEmu VM with libvirt.
+
+Questions? Comments?
+
+Find me here:
+
+- [Twitter](https://twitter.com/therealhellcat)
+- [TikTok](https://www.tiktok.com/@thereallyrealhellcat)
---
+++ rm-portfwd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "x$3" == "x" ]; then
+  echo " "
+  echo "  usage: $0 srcport dstport dstaddr"
+  echo " "
+  echo "    srcport = public port requests are comming in at"
+  echo "    dstport = port on VM to forward traffic to"
+  echo "    dstaddr = IP address of VM to forward traffic to"
+  echo " "
+  exit 1
+fi
+
+
+HOSTIP="10.11.12.13" # !!! change this to the actual IP address of your VM host
+SRCPORT=$1
+DSTPORT=$2
+DSTADDR=$3
+
+iptables -t nat -D PREROUTING -p tcp -d $HOSTIP --dport $SRCPORT -j DNAT --to $DSTADDR:$DSTPORT
+iptables -D FORWARD -d $DSTADDR/32 -p tcp -m state --state NEW -m tcp --dport $DSTPORT -j ACCEPT
